9cf04e368aa97ab20a7c82438a932d4f00dc5a1c
pengju1218 <920988751@qq.com>
2017-01-26 17:06:33 +0800

    Signed-off-by: pengju1218 <920988751@qq.com>

---
+++ content/2017.01.13/javascript mvvm/移动web/Sencha Touch(不好)/zx/app.js
@@ -0,0 +1,80 @@
+﻿
+Ext.application({
+	id:'itKingApp',
+	
+	launch:function(){
+		var formPanel=Ext.create('Ext.form.Panel',{
+			id:'formPanel1',
+			// 表示是否可以滑动，vertical=上下滑动 horizontal=左右滑动 both=上下左右都可以 flase=不滑动
+			scrollable:'vertical',
+			width:'700px',
+			height:'400px',
+			centered:true,
+			style:{
+				'padding':'1px'
+			},
+			items:[{
+				xtype:'fieldset',
+				title:'用户信息',
+				instructions:'请填写用户信息',
+
+				width:'640px',
+				height:'180px',
+				defaults:{
+					// 主要设置了textfield的label长度，这里固定了150px，也可以设置百分比
+					labelWidth:'150px'
+				},
+				items:[{
+					xtype:'textfield',
+					id:'txt_name',
+					namle:'name',
+					label:'用户名',
+					placeHolder:'请输入用户名',
+					// required=true label旁边有个* false则没有
+					required:true,
+					// 输入框中右侧的X按钮
+					clearIcon:true,
+					// 显示的label位置，有上下左右四种，默认是left（top， bottom，right）
+					labelAlign:'left'
+				},
+				{
+					xtype:'passwordfield',
+					id:'txt_password',
+					name:'password',
+					label:'密码',
+					placeHolder:'请输入密码',
+					required:true,
+					clearIcon:true
+				}]
+			},
+			{
+				xtype:'container',
+				layout:{
+					type:'vbox'
+				},
+				items:[
+				{
+					xtype:'button',
+					text:'注册',
+					width:'600px',
+					style:{
+						'margin-left':'30px'
+					}
+				},
+				{
+					xtype:'button',
+					text:'登陆',
+					width:'600px',
+					style:{
+						'margin-left':'30px',
+						'margin-top':'10px'
+					}
+				}
+				]
+			}]
+		});
+		
+		Ext.Viewport.add(formPanel);
+	}
+	
+})
---
+++ content/2017.01.13/javascript mvvm/移动web/Sencha Touch(不好)/zx/nav.js
@@ -0,0 +1,29 @@
+﻿
+Ext.application({
+	name:'itkingApp',
+	launch:function(){
+		var view =Ext.create('Ext.NavigationView',{
+			items:[
+			{
+				title:'title1',
+				html:'one'
+			}]
+		});
+		
+		var panel=Ext.create('Ext.Panel',{
+			title:'title2',
+			html:'two'
+		});
+		
+		var panel2=Ext.create('Ext.Panel',{
+			title:'title3',
+			html:'three'
+		})		
+		
+		Ext.Viewport.add(view);
+		view.push(panel);
+		view.push(panel2);		
+		
+	}
+	
+})
